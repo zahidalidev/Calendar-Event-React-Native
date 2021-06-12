7d8cc7298d7657055a016ee6b79daa34a9267cb2
zahidalidev <m.zahidalidev@gmail.com>
2021-06-12 06:05:29 +0500

    working on both ios and android

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import * as Permissions from 'expo-permissions';
+import * as Calendar from 'expo-calendar';
+
+
 import Home from './screens/Home';
 import CreateTask from './screens/CreateTask';
 import TodoStore from './data/TodoStore';
@@ -13,17 +16,25 @@ LogBox.ignoreAllLogs();
 
 const Stack = createDrawerNavigator();
 
-export default class App extends Component {
-  async UNSAFE_componentWillMount() {
-    await this._askForCalendarPermissions();
-    await this._askForReminderPermissions();
-  }
+export default function App() {
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === "ios") {
+        await Calendar.requestRemindersPermissionsAsync()
+        await Calendar.requestCalendarPermissionsAsync()
+      } else {
+        await _askForCalendarPermissions();
+        await _askForReminderPermissions();
+      }
+    })();
+  }, []);
 
-  _askForCalendarPermissions = async () => {
+
+  const _askForCalendarPermissions = async () => {
     await Permissions.askAsync(Permissions.CALENDAR);
   };
 
-  _askForReminderPermissions = async () => {
+  const _askForReminderPermissions = async () => {
     if (Platform.OS === 'android') {
       return true;
     }
@@ -31,25 +42,25 @@ export default class App extends Component {
     await Permissions.askAsync(Permissions.REMINDERS);
   };
 
-  render() {
-    return (
-      <TodoStore>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home"
-            drawerType={"front"}
-            overlayColor="transparent"
-            edgeWidth={100}
-            drawerStyle={{
-              backgroundColor: "white",
-              width: 0
-            }}
+  return (
+    <TodoStore>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home"
+          drawerType={"front"}
+          overlayColor="transparent"
+          edgeWidth={100}
+          drawerStyle={{
+            backgroundColor: "white",
+            width: 0
+          }}
 
-          >
-            <Stack.Screen initialParams={{ updateCurrentTask: "", currentDate: new Date(), createNewCalendar: '' }} name="Home">{(props) => <Home {...props} />}</Stack.Screen>
-            <Stack.Screen initialParams={{ updateCurrentTask: "", currentDate: new Date(), createNewCalendar: '' }} name="CreateTask">{(props) => <CreateTask {...props} />}</Stack.Screen>
-          </Stack.Navigator>
-        </NavigationContainer>
-      </TodoStore>
-    );
-  }
+        >
+          <Stack.Screen initialParams={{ updateCurrentTask: "", currentDate: new Date(), createNewCalendar: '' }} name="Home">{(props) => <Home {...props} />}</Stack.Screen>
+          <Stack.Screen initialParams={{ updateCurrentTask: "", currentDate: new Date(), createNewCalendar: '' }} name="CreateTask">{(props) => <CreateTask {...props} />}</Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </TodoStore>
+  );
+
 }
+
